3cc122eea646e76da00d169651b8caa903b9fff9
hrmrakib <hrmrakibs@gmail.com>
2024-12-03 22:05:35 +0600

    try to fix deploy err

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
-import DateRangePicker from "@/components/DateRangePicker";
+// import DateRangePicker from "@/components/DateRangePicker";
 
 const Home = () => {
   return (
     <div className='container mx-auto min-h-screen px-4 py-8'>
       <h1 className='text-3xl font-bold mb-8 text-center'>Date Range Picker</h1>
-      <div className='flex justify-center'>
-        <DateRangePicker />
-      </div>
+      <div className='flex justify-center'>{/* <DateRangePicker /> */}</div>
     </div>
   );
 };
